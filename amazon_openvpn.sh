93fb1110c0a6205c1cc786f886b2c7f1a0eb7ad7
gultz <gultz5102@kakao.com>
2024-08-27 17:16:23 +0900

    enable aws dns

--- amazon_openvpn.sh
+++ amazon_openvpn.sh
@@ -180,13 +180,15 @@ server 10.8.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt" >>/etc/openvpn/server.conf
 
 
-	#echo 'push "redirect-gateway def bypass-dhcp"' >>/etc/openvpn/server.conf	
+	#echo 'push "redirect-gateway def bypass-dhcp"' >>/etc/openvpn/server.conf
 
 	#use AWS DNS 
-	#IFS='.' read -r -a ip_array <<< "$VPC_RANGE"
-	#ip_array[3]=2  # 마지막 숫자를 2로 설정
+	IFS='.' read -r -a ip_array <<< "$VPC_RANGE"
+	ip_array[3]=2  
 
-	#NEW_IP="${ip_array[0]}.${ip_array[1]}.${ip_array[2]}.${ip_array[3]}"
+	NEW_IP="${ip_array[0]}.${ip_array[1]}.${ip_array[2]}.${ip_array[3]}"
+	echo 'push "dhcp-option DNS 8.8.8.8"' >>/etc/openvpn/server.conf
+
 
 	#echo "push \"dhcp-option DNS $NEW_IP\"" >> /etc/openvpn/server.conf
 
